0e5326da41614d04ac43bb70654879ef025b19b9
logicfish <me@logicfish.me.uk>
2016-10-06 21:44:05 +0100

    GPL

--- source/drete/package.d
+++ source/drete/package.d
@@ -3,12 +3,12 @@ Copyright 2016 Mark Fisher
 
 This file is part of DRete.
 
-Foobar is free software: you can redistribute it and/or modify
+DRete is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Foobar is distributed in the hope that it will be useful,
+DRete is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
